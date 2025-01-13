1615d62e432ca8fbf99ba912b23e62dbf845c810
Akem-Ben <akemini.ndaobong@gmail.com>
2025-01-13 15:03:57 +0100

    New build

--- build/utilities/uploads/cloudinary.utilities.js
+++ build/utilities/uploads/cloudinary.utilities.js
@@ -8,7 +8,6 @@ const multer_storage_cloudinary_1 = require("multer-storage-cloudinary");
 const cloudinary_1 = require("cloudinary");
 const dotenv_1 = __importDefault(require("dotenv"));
 const envKeys_1 = require("../../configurations/envKeys");
-const logger_utilities_1 = require("../logger/logger.utilities");
 dotenv_1.default.config();
 cloudinary_1.v2.config({
     cloud_name: envKeys_1.CLOUDINARY_NAME,
@@ -26,7 +25,7 @@ const storage = new multer_storage_cloudinary_1.CloudinaryStorage({
             };
         }
         catch (error) {
-            logger_utilities_1.logger.error(`Cloudinary storage error: ${error.message}`);
+            console.error(`Cloudinary storage error: ${error.message}`);
             throw error;
         }
     }
@@ -43,13 +42,13 @@ const cloud = (0, multer_1.default)({
                 cb(null, true);
             }
             else {
-                logger_utilities_1.logger.error(`Invalid file type: ${file.mimetype}`);
+                console.error(`Invalid file type: ${file.mimetype}`);
                 cb(null, false);
                 return cb(new Error("Only .png, .jpg, .jpeg, .webp, .avif formats are allowed"));
             }
         }
         catch (error) {
-            logger_utilities_1.logger.error(`File filter error: ${error.message}`);
+            console.error(`File filter error: ${error.message}`);
             cb(error);
         }
     },
@@ -62,7 +61,7 @@ const upload = (req, res, next) => {
     cloud(req, res, (error) => {
         if (error instanceof multer_1.default.MulterError) {
             // A Multer error occurred when uploading
-            logger_utilities_1.logger.error(`Multer error: ${error.message}`);
+            console.error(`Multer error: ${error.message}`);
             return res.status(400).json({
                 status: 'error',
                 message: error.message
@@ -70,7 +69,7 @@ const upload = (req, res, next) => {
         }
         else if (error) {
             // An unknown error occurred when uploading
-            logger_utilities_1.logger.error(`Upload error: ${error.message}`);
+            console.error(`Upload error: ${error.message}`);
             return res.status(500).json({
                 status: 'error',
                 message: error.message
